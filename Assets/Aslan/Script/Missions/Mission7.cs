27fd858efe326ff0c09b509dc278d58fec88fd66
shinyi866 <littlemoivetheater@gmail.com>
2021-04-02 19:34:00 +0800

    Modify mission7

--- Assets/Aslan/Script/Missions/Mission7.cs
+++ Assets/Aslan/Script/Missions/Mission7.cs
@@ -2,42 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using View;
+using Hsinpa.Ctrl;
+using UnityEngine.XR.ARFoundation;
 
 namespace GameMission
 {
     public class Mission7 : Mission
     {
+        public GameObject hisnpaPrefab;
+        public CrocoBoneRepairCtrl crocoCtrl;
+        public ARPlaneManager planeManager;
 
         private int missionIndex = 7;
 
         public override void EnterGame()
         {
-
+            GameModals.instance.OpenAR();
+            planeManager.enabled = true;
+            hisnpaPrefab.SetActive(true);
         }
 
         public override void StartGame()
         {
             var modal = GameModals.instance.OpenModal<ARGameModal>();
+            modal.ShowModal(missionIndex, TypeFlag.ARGameType.Original);
 
-            // step 1
-            modal.ShowPrompt(missionIndex, TypeFlag.ARGameType.GamePrompt1);
-
-            // step 2
-            modal.ShowPrompt(missionIndex, TypeFlag.ARGameType.GamePrompt2);
-
-            // step 3
-            modal.ShowPrompt(missionIndex, TypeFlag.ARGameType.GamePrompt3);
-
-            // step 4
-            modal.ShowPrompt(missionIndex, TypeFlag.ARGameType.GamePrompt4);
-
-            // step 5
-            modal.ShowPrompt(missionIndex, TypeFlag.ARGameType.GamePrompt5);
+            crocoCtrl.OnEndGameEvent += EndGame;
+            crocoCtrl.EnterGame();
         }
 
         public void EndGame(bool isSuccess)
         {
+            crocoCtrl.OnEndGameEvent -= EndGame;
 
+            var model = GameModals.instance.OpenModal<DialogModal>();
+            model.ShowInfo(missionIndex, TypeFlag.DialogType.EndDialog);
+
+            model.ConfirmButton.onClick.AddListener(() =>
+            {
+                Games.instance.ClosGame();
+                MediaPlayerController.instance.CloseVideo();
+                GameModals.instance.CloseModal();
+                GameModals.instance.GetBackAnimalAR(missionIndex, TypeFlag.ARObjectType.Animals);
+            });
         }
     }
 }
